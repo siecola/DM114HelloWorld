3861b6a63570f0f8580b66fdc0c84d912983a63c
siecola <siecola@gmail.com>
2018-03-22 20:08:32 -0300

    Adding graphical components and their logic

--- app/src/main/java/br/com/siecola/helloworld/MainActivity.java
+++ app/src/main/java/br/com/siecola/helloworld/MainActivity.java
@@ -2,6 +2,10 @@ package br.com.siecola.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!editText.getText().toString().isEmpty())
+                    textView.setText(editText.getText().toString());
+            }
+        });
     }
 }
